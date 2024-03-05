d499325a76330629590217190478b727833bc04b
Aaron Schmid <aaron@clab.rocks>
2024-03-05 11:00:36 +0100

    inital

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "A flake with pnpm dev shell";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShells.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          openssl
+          nodejs_18
+          nodePackages.pnpmx
+        ];
+       };
+    });
+}
